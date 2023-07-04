98057b897f6b3d127210394b88451050d8422445
agoodmu <agoodmu@gmail.com>
2023-07-04 15:16:03 +0800

    update policy

--- checkov-policy/network/sg.py
+++ checkov-policy/network/sg.py
@@ -2,17 +2,15 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
 
-class UnLimitedIngressTraffic(BaseResourceCheck):
-    def __init__(self, check_id, port,ipaddress=None):
-        name = "Ensure no security groups allow ingress from %s to port %d" % (ipaddress,port)
+class UnRestrictedIngressTraffic(BaseResourceCheck):
+    def __init__(self, check_id, port):
+        name = "Ensure no security groups allow ingress from 0.0.0.0 to port %d" % port
         supported_resources = ['huaweicloud_networking_secgroup_rule']
         categories = [CheckCategories.NETWORKING]
         super().__init__(name=name, id=check_id, categories=categories, supported_resources=supported_resources)
         self.port = port
-        self.ipaddress = ipaddress
-
+        
     def scan_resource_conf(self, conf: dict[str,list[any]]) -> CheckResult:
-        self.ipaddress = conf['remote_ip_prefix']
         """
             Looks for configuration at security group ingress rules :
             https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/networking_secgroup_rule
@@ -32,34 +30,27 @@ class UnLimitedIngressTraffic(BaseResourceCheck):
         if conf['direction'][0] == 'egress':  # This means it's an huaweicloud_networking_secgroup_rule egress resource.
             return CheckResult.PASSED
         
-        """
-        check if the remote ip address is allowed
-        """
-        if self.ipaddress != None:
-            if type(self.ipaddress) is list:
-                if 'remote_ip_prefix' in conf:
-                    if conf['remote_ip_prefix'][0] in self.ipaddress:
-                        return CheckResult.FAILED
-            else:
-                if 'remote_ip_prefix' in conf:
-                    if conf['remote_ip_prefix'][0] == self.ipaddress:
-                        return CheckResult.FAILED
-
-        """
-        check if port is allowed
-        """
-        if 'port_range_min' in conf:
-            if int(conf['port_range_min'][0]) <= int(self.port) <= int(conf['port_range_max'][0]):
-                return CheckResult.FAILED
+        # This means if this rule's effect is deny and no need to check it
+        if 'action' in conf and conf['action'][0] == 'deny':
+            return CheckResult.PASSED
+        
+        # This means this group uses ip address group which doesn't allow 0.0.0.0/0
+        if 'remote_ip_prefix' not in conf:
+            return CheckResult.PASSED
+        
+        if conf['remote_ip_prefix'][0] == '0.0.0.0/0':
+            if 'port_range_min' in conf:
+                if int(conf['port_range_min'][0]) <= int(self.port) <= int(conf['port_range_max'][0]):
+                    return CheckResult.FAILED
             
-        if 'ports' in conf:
-            if str(self.port) in str.split(conf['ports'][0],","):
-                return CheckResult.FAILED
+            if 'ports' in conf:
+                if str(self.port) in str.split(conf['ports'][0],","):
+                    return CheckResult.FAILED
         
         return CheckResult.PASSED
     
 
-class SecurityGroupUnrestrictedIngress22(UnLimitedIngressTraffic):
+class SecurityGroupUnrestrictedIngress22(UnRestrictedIngressTraffic):
     def __init__(self):
         super().__init__(check_id="HW_NETWORK_001", port=22)
     
